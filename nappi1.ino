ec5859d4005dc023cf854ed2f1e2601eb5168465
Arvo Vimpari <Arppavim2@gmail.com>
2023-10-29 08:53:29 +0200

    first commit

---
+++ nappi1.ino
@@ -0,0 +1,31 @@
+
+// määritellään muuttujat Ledeille
+const int nappiPin = 2; //vakio, painonapin pinni
+int ledVih = 13;//muuttujan ledVih arvokasi annetaan 13 (pinni)
+int napinTila = 0; //alustetaan painonappi
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // asetetaan pinnit outputeiksi.
+pinMode(ledVih, OUTPUT);//13
+ // asetetaan painonappi inputiksi
+pinMode(nappiPin, INPUT);
+
+}
+
+// vilkutellaan ledejä
+void loop() 
+{
+  napinTila = digitalRead(nappiPin); //luetaan painonapin tila muuttujaan 
+
+  if(napinTila == HIGH)
+  {
+    digitalWrite(ledVih, HIGH);//sytytetään vihreä ledi
+    
+  }
+  else
+  {
+    digitalWrite(ledVih, LOW); //sammutetaan vihreä ledi
+  }
+
+} 
+
