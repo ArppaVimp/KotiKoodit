eed566fd1b9cdc04d3c156b6c45fc96f70b10343
Arvo Vimpari <Arppavim2@gmail.com>
2023-11-01 09:13:29 +0200

    commit - tehtiin pai muutosta

--- nappi1/nappi1.ino
+++ nappi1/nappi1.ino
@@ -1,7 +1,7 @@
 
 // määritellään muuttujat Ledeille
 const int nappiPin = 2; //vakio, painonapin pinni
-int ledVih1 = 13;//muuttujan ledVih arvokasi annetaan 13 (pinni)
+int ledVih1 = 12;//muuttujan ledVih arvokasi annetaan 13 (pinni)
 int napinTila = 0; //alustetaan painonappi
 // the setup function runs once when you press reset or power the board
 void setup() {
